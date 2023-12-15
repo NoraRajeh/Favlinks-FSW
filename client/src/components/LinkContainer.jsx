c67c519416bb50ad7e8d6f5108e94621854358b4
NoraRajeh <139821640+NoraRajeh@users.noreply.github.com>
2023-12-15 03:07:41 +0300

    update client to CURD by API

--- client/src/components/LinkContainer.jsx
+++ client/src/components/LinkContainer.jsx
@@ -1,28 +1,139 @@
 import Table from './Table'
 import Form from './Form'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+
+
+
 
 function LinkContainer(){
+    useEffect(() => {
+        getLinks()
+    },[]);
     const [favLinks, setFavLinks] = useState([])
+    const [focus, setFocus] = useState(true)
+    const getLinks = async ()=>{
+        try{
+            const response = await fetch('/api/links')
+            const data = await response.json()
+            const n_data = data['rows'].map((x)=>{
+                return {
+                    URL: x.url,
+                    name: x.name,
+                    id: x.id
+                }
+            })
+            setFavLinks(n_data)
+        }catch(e){
+            console.log(e)
+        }
+    }
 
+    const submitUpdate = (e)=>{
+        e.preventDefault()
+        const p_fav = favLinks[idx]
+        fetch(`/api/links/${p_fav.id}`, {
+        method: 'PUT',
+        body: JSON.stringify({
+            name: name,
+            url: URL,
+        }),
+        headers: { 'Content-Type': 'application/json' },
+        })
+        .then(() => {
+            p_fav.URL = URL
+            p_fav.name = name
+            setFavLinks([...favLinks])
+            setIsViable(false)
+        })
+        .catch(() => {
+            alert('Has Error While update try please again')
+        });
+    }
+
+    function UpdateForm(){
+        return <div id='update_from' 
+                style={{position:'absolute', width:'100%', 
+                        height:'99vh', display:'flex', top:'0',
+                        left:'0', bottom:'0', justifyContent:'center', 
+                        alignItems:'center'}}>
+                        <div style={{backgroundColor:'#eee', width:'50%', height:'10rem', borderRadius:'.4rem', 
+                            padding:'1rem'}}>
+                        <form style={{ display:'flex', flexDirection:'column', justifyContent:'space-between', height:'100%'}}>
+                            <div>
+                                <label htmlFor="uname">Name:</label>
+                                <br/>
+                                {
+                                    focus?<input type="text" name="uname" id="uname" autoFocus value={name} onChange={(newVal)=>{setName(newVal.target.value);setFocus(true)}} style={{width:"99%", height:"1.8rem", borderRadius:".3rem", border:"solid 1px #888"}}/>
+                                    :<input type="text" name="uname" id="uname" value={name} onChange={(newVal)=>{setName(newVal.target.value);setFocus(true)}} style={{width:"99%", height:"1.8rem", borderRadius:".3rem", border:"solid 1px #888"}}/>
+                                }
+                                
+                            </div>
+                            <div>
+                                <label htmlFor="uurl">URL:</label>
+                                <br/>
+                                {
+                                    !focus?<input type="text" name="uurl" id="uurl" autoFocus value={URL} onChange={(newVal)=>{setURL(newVal.target.value);setFocus(false)}} style={{width:"99%", height:"1.8rem", borderRadius:".3rem", border:"solid 1px #888"}}/>
+                                    :<input type="text" name="uurl" id="uurl" value={URL} onChange={(newVal)=>{setURL(newVal.target.value);setFocus(false)}} style={{width:"99%", height:"1.8rem", borderRadius:".3rem", border:"solid 1px #888"}}/>
+                                }
+                                
+                            </div>
+                            <div style={{display:'flex'}}>
+                                <input type="submit" value="Update" onClick={submitUpdate} style={{width:"7rem", height:"2rem", backgroundColor:"#1447FD", color:"white", borderRadius:".3rem", border:"none"}}></input>
+                                <div style={{width:'1rem'}}></div>
+                                <input type="submit" value="Cancel" onClick={(t)=>{t.preventDefault();setIsViable(false)}}  style={{width:"7rem", height:"2rem", backgroundColor:"#1447FD", color:"white", borderRadius:".3rem", border:"none"}}></input>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+    }
+
+
+    const [name, setName] = useState("")
+    const [URL, setURL] = useState("")
+    const [idx, setIdx] = useState("")
+    const [isViable, setIsViable] = useState(false)
+    
     const handleRemove = (index) => {
-        setFavLinks([...(favLinks.filter((_, idx)=> idx != index))])
+        fetch(`/api/links/${favLinks[index].id}`, {method: 'DELETE'})
+        .then(() => {setFavLinks([...(favLinks.filter((_, idx)=> idx != index))])})
+        .catch(() => {
+            alert('Has Error While Delete try please again')
+        });
+        
+    }
+
+    const handleEdit = (index) =>{
+        setIdx(index)
+        setIsViable(true)
     }
     
     const handleSubmit = (favLink) => {
-        setFavLinks([...favLinks, favLink])
+        fetch('/api/links/', {
+            method: 'POST',
+            body: JSON.stringify({
+                name: favLink.name,
+                url: favLink.URL,
+            }),
+            headers: { 'Content-Type': 'application/json' },
+        }).then((response)=>{
+            favLink.id = response.rows[0].id
+            setFavLinks([...favLinks, favLink])
+        }).catch(()=>{
+            alert('Has Error While Create try please again')
+        })
     }
 
     return(
-        <div style={{padding: "1rem 6rem", }}>
-            <h1>My Favorite Links</h1>
-            <p>Add a new link with a name and URL to the table! </p>
-            <Table linkData={favLinks} removeLink={handleRemove}/>
-            <h1>Add New</h1>
-            <Form handleSubmit={handleSubmit}/>
-            {/* <div style={{height:"5rem", width:"100%"}}></div> */}
-            {/* <div style={{backgroundColor:"black", textAlign:"center" }}>hello</div> */}
-        </div>
+        <>
+            <div style={{padding: "1rem 6rem", }}>
+                <h1>My Favorite Links</h1>
+                <p>Add a new link with a name and URL to the table! </p>
+                <Table linkData={favLinks} removeLink={handleRemove} editLink={handleEdit}/>
+                <h1>Add New</h1>
+                <Form handleSubmit={handleSubmit} />
+            </div>
+            {isViable?<UpdateForm />:<></>}
+        </>
     )
 
 }
--- client/src/components/Table.jsx
+++ client/src/components/Table.jsx
@@ -1,45 +1,49 @@
 
 function TableHeader(){
-    /* responsible for rendering the head of our table with the appropriate columns */
-    return(
-        <thead>
-        <tr style={{ borderBottom:"solid 1px #ccc" }}>
-          <th>Name</th>
-          <th>URL</th>
-          <th>Remove</th>
-        </tr>
-      </thead>
-    )
+  /* responsible for rendering the head of our table with the appropriate columns */
+  return(
+      <thead>
+      <tr style={{ borderBottom:"solid 1px #ccc" }}>
+        <th>Name</th>
+        <th>URL</th>
+        <th>Remove</th>
+        <th>Edit</th>
+      </tr>
+    </thead>
+  )
 }
 
 const TableBody = (props) => {
-    // boilerplate table body functional component
-    // we use Array.map to create table rows from LinkData passed via props
-    const rows = props.linkData.map((row, index) => {
-      return (
-        <tr key={index} style={{ borderBottom:"solid 1px #ccc"}}>
-          <td style={{padding:".8rem"}}>{row.name}</td>
-          <td>
-            <a href={row.URL}>{row.URL}</a>
-          </td>
-          <td>
-            <button onClick={() => props.removeLink(index)} style={{width:"5rem", height:"1.8rem", backgroundColor:"#1447FD", color:"white", borderRadius:".3rem", border:"none"}}>Delete</button>
-          </td>
-        </tr>
-      )
-    })
-  
-    return <tbody>{rows}</tbody>
-  }
+  // boilerplate table body functional component
+  // we use Array.map to create table rows from LinkData passed via props
+  const rows = props.linkData.map((row, index) => {
+    return (
+      <tr key={index} style={{ borderBottom:"solid 1px #ccc"}}>
+        <td style={{padding:".8rem"}}>{row.name}</td>
+        <td>
+          <a href={row.URL}>{row.URL}</a>
+        </td>
+        <td>
+          <button onClick={() => props.removeLink(index)} style={{width:"5rem", height:"1.8rem", backgroundColor:"#1447FD", color:"white", borderRadius:".3rem", border:"none"}}>Delete</button>
+        </td>
+        <td>
+          <button onClick={() => props.editLink(index)} style={{width:"5rem", height:"1.8rem", backgroundColor:"#1447FD", color:"white", borderRadius:".3rem", border:"none"}}>Edit</button>
+        </td>
+      </tr>
+    )
+  })
+
+  return <tbody>{rows}</tbody>
+}
 
 
 function Table(props){
-    return(
-        <table style={{width:"100%", textAlign: "left", borderCollapse: "collapse"}}>
-        <TableHeader/>
-        <TableBody linkData={props.linkData} removeLink={props.removeLink} />
-        </table>
-    )
+  return(
+      <table style={{width:"100%", textAlign: "left", borderCollapse: "collapse"}}>
+      <TableHeader/>
+      <TableBody linkData={props.linkData} removeLink={props.removeLink} editLink={props.editLink}/>
+      </table>
+  )
 }
 
 export default Table
